d492361383842b9ddb0e94d69e4b9df445c3e557
Carlos <unaltrataronja@gmail.com>
2021-03-31 14:39:05 +0200

    Clases DB actualizadas
    Se han actualizado las clases DB para que contengan todos los métodos con código SQL. Al realizar esta operación, las clases que almacenaban los CRUD se han eliminado ya que no servían para nada

--- src/mainclasses/database/ProjectDB.java
+++ src/mainclasses/database/ProjectDB.java
@@ -1,12 +1,22 @@
 package mainclasses.database;
 
+import auxiliar.CustomException;
 import auxiliar.DatabaseConnection;
+import auxiliar.Error;
+import auxiliar.InputOutput;
+import auxiliar.Log;
+import custom_ui.tables.CustomTableConfig;
+import custom_ui.tables.CustomTableModel;
+import gui.dialogs.ProjectDialog;
+import gui.dialogs.ShowProjects;
 import mainclasses.entity.Company;
 import mainclasses.entity.School;
 import mainclasses.proposal.Project;
 import mainclasses.proposal.Proposal;
 import mainclasses.user.Employee;
 
+import javax.swing.*;
+import java.io.IOException;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
@@ -123,7 +133,7 @@ public class ProjectDB {
                     school = new School(idSchool, entityName, city, phone, territorialId);
                     proposal = new Proposal(idProposal, title, description, date, school);
 
-                    this.addProposal(proposal);
+                    //this.addProposal(proposal);
                 } else {
                     idProposal = rs.getInt("idproposal");
                     title = rs.getString("title");
@@ -139,11 +149,106 @@ public class ProjectDB {
                     company = new Company(idCompany, entityName, city, phone, cif);
                     proposal = new Proposal(idProposal, title, description, date, company);
 
-                    this.addProposal(proposal);
+                    //this.addProposal(proposal);
                 }
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    /**
+     * Permite crear Proyectos
+     * @param proposalTable JTable con propuestas
+     * @param proposalList Objeto de la clase ProposalDB con la lista de propuestas
+     * @param title String con título de la propuesta
+     * @param description String con la descripción de la propuesta
+     * @throws IOException Excepción de entrada/salida
+     */
+    public void createProject(JTable proposalTable, ProposalDB proposalList, String title, String description) throws CustomException, IOException {
+        // Panel con formulario y comboBox
+        ProjectDialog projectDialog = new ProjectDialog(title, description);
+
+        // Almacena un entero, necesario para Diálogo de confirmación
+        int resultado;
+
+        // Fila seleccionada
+        int selectedRow = proposalTable.getSelectedRow();
+
+        try {
+            // Si no hay empleados creados
+            if (projectDialog.getCbEmployee() == null) {
+                throw new CustomException(1115);
+
+                // Si no hay filas seleccionadas en la tabla de propuestas
+            } else if (selectedRow < 0) {
+                throw new CustomException(1114);
+            } else {
+                // Mostramos diálogo de confirmación
+                resultado = JOptionPane.showConfirmDialog(null, projectDialog, "CREAR PROYECTO", JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
+
+                // No se puede utilizar un listener si utilizamos showxxxxDialog
+                if (resultado == 0) {
+                    // Si el resultado es 0, significa que el usuario ha hecho clic en OK.
+                    Project project = null; //new Project(projectDialog.getTxtTitle(), projectDialog.getTxtDescription(), projectDialog.getCbEmployee());
+
+                    // Añadimos proyecto
+                    this.addProject(project);
+
+                    // Añadimos la entrada al log
+                    Log.capturarRegistro("PROJECT CREATE " + project.toString());
+
+                    // Añadimos la entrada, antes de eliminar la propuesta para obtener el valor de la fila seleccionada
+                    Log.capturarRegistro("PROPOSAL DELETE " + proposalList.getProposalFromDB(selectedRow));
+
+                    // Al crear proyecto, eliminamos la propuesta
+                    proposalList.removeProposal(selectedRow);
+
+                    // Refrescamos la tabla para eliminar la propuesta de ahí también
+                    this.showData(proposalTable);
+                }
+            }
+        } catch (CustomException ce) {
+            InputOutput.printAlert(ce.getMessage());
+
+            // Capturamos error para el registro
+            Error.capturarError("PROJECT " + ce.getMessage());
+        }
+    }
+
+
+    /**
+     * Muestra los datos actualizados en la tabla de proyectos
+     */
+    public void showData(JTable proposalTable) throws IOException {
+        int resultado;
+
+        // Implementa panel para visualizar proyectos
+        ShowProjects showProjects = new ShowProjects();
+
+        // Creamos array de tipo string e inicializamos con el tamaño del ArrayList
+        String[][] tabla = new String[this.sizeProjectDB()][5];
+
+        // Recorre la lista de Proyectos
+        for(int i = 0; i < this.sizeProjectDB(); i++) {
+            // Datos de cada Proyecto
+            tabla[i][0] = this.getProjectFromDB(i).getName();
+            tabla[i][1] = this.getProjectFromDB(i).getDescription();
+            tabla[i][2] = this.getProjectFromDB(i).getManager().getName();
+        }
+
+        // Añade los datos al modelo
+        showProjects.getProjectTable().setModel(new CustomTableModel(
+                tabla,
+                new String [] {
+                        "Título", "Descripción", "Manager"
+                }
+        ));
+
+        // Diseño de la tabla
+        CustomTableConfig.initConfig(showProjects.getProjectTable());
+
+        // Mostramos diálogo de confirmación
+        JOptionPane.showConfirmDialog(null, showProjects, "VER PROYECTOS", JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
     }
 }
--- src/mainclasses/database/ProposalDB.java
+++ src/mainclasses/database/ProposalDB.java
@@ -1,13 +1,23 @@
 package mainclasses.database;
 
+import auxiliar.CustomException;
 import auxiliar.DatabaseConnection;
 
+import auxiliar.Error;
+import auxiliar.InputOutput;
+import auxiliar.Log;
+import custom_ui.tables.CustomTableConfig;
+import custom_ui.tables.CustomTableModel;
 import mainclasses.entity.Company;
 import mainclasses.entity.School;
 import mainclasses.proposal.Proposal;
 
+import javax.swing.*;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
+import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Date;
 
@@ -145,5 +155,210 @@ public class ProposalDB {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    /**
+     * Permite crear propuestas
+     *
+     * @param proposalTable tabla donde se visualizan las propuestas
+     * @param title         nombre de la propuesta
+     * @param description   descripción de la propuesta
+     * @param startDate     fecha de la propuesta
+     * @param entity        entidad de la propuesta
+     */
+    public void createProposal(JTable proposalTable, String title, String description, String startDate, int entity) {
+        String sql = "INSERT INTO PROPOSALS (title, description, startdate, creationdate, status, identity) VALUES (?,?,?,?,?,?)";
+
+        try (PreparedStatement stmt = DatabaseConnection.getConnection().prepareStatement(sql)) {
+            // Si hay algún campo vacío
+            if (title.isEmpty() || description.isEmpty() || startDate.isEmpty() || entity == 0) {
+                throw new CustomException(1111);
+
+            } else {
+                // Si la fecha introducida es anterior a la actual
+                if (InputOutput.wrongDate(startDate)) {
+                    throw new CustomException(1117);
+
+                } else {
+                    stmt.setString(1, title);
+                    stmt.setString(2, description);
+                    stmt.setString(3, startDate);
+
+                    stmt.setString(4, InputOutput.todayDate());
+                    stmt.setString(5, "active");
+                    stmt.setObject(6, entity);
+
+                    stmt.executeUpdate();
+
+                    // Añadimos la entrada al log
+                    Log.capturarRegistro("PROPOSAL CREATE " + title + " " + description + " " + startDate);
+
+                    // Actualizamos los datos en la tabla
+                    showData(proposalTable);
+                }
+            }
+        } catch (CustomException ce) {
+            InputOutput.printAlert(ce.getMessage());
+
+            // Capturamos error para el registro
+            auxiliar.Error.capturarError("PROPOSAL " + ce.getMessage());
+
+        } catch (ParseException pe) {
+            String alerta = "Error: Fecha con formato desconocido";
+            InputOutput.printAlert(alerta);
+
+            // Capturamos error para el registro
+            auxiliar.Error.capturarError("PROPOSAL " + alerta);
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+
+    /**
+     * Permite modificar propuestas
+     * @param proposalTable tabla donde se visualizan las propuestas
+     * @param title         nombre de la propuesta
+     * @param description   descripción de la propuesta
+     * @param startDate     fecha de la propuesta
+     * @param indexEntity   indice del objeto del JCombobox
+     */
+    public void editProposal(JTable proposalTable, String title, String description, String startDate, int indexEntity, int idProposal) {
+        String sql = "UPDATE PROPOSALS SET title = ?, description = ?, startDate = ?, creationdate = ?, identity = ? WHERE idproposal = ?";
+
+        try {
+            // Si los campos están vacíos
+            if (title.isEmpty() || description.isEmpty() || startDate.isEmpty() || indexEntity == -1) {
+                throw new CustomException(1111);
+
+                // Si la fecha introducida es anterior a HOY
+            } else if (InputOutput.wrongDate(startDate)) {
+                throw new CustomException(1117);
+
+            } else {
+                try (PreparedStatement stmt = DatabaseConnection.getConnection().prepareStatement(sql)) {
+
+                    stmt.setString(1, title);
+                    stmt.setString(2, description);
+                    stmt.setString(3, startDate);
+                    stmt.setString(4, InputOutput.todayDate());
+                    stmt.setInt(5, indexEntity);
+
+                    stmt.setInt(6, idProposal);
+
+                    stmt.executeUpdate();
+                    //stmt.close(); // No hace falta con el try-with-resources
+
+                    // Añadimos la entrada al log
+                    Log.capturarRegistro("PROPOSAL EDIT " + title + " "
+                            + description + " "
+                            + InputOutput.stringToDate(startDate) + "  "
+                            + "ENTITY ID " + indexEntity);
+
+                    // Actualizamos datos de la tabla
+                    showData(proposalTable);
+
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        } catch (CustomException ce) {
+            InputOutput.printAlert(ce.getMessage());
+
+            // Capturamos error para el registro
+            auxiliar.Error.capturarError("PROPOSAL " + ce.getMessage());
+        } catch (ParseException e) {
+            InputOutput.printAlert("Error: La fecha introducida no es válida");
+        }
+
+    }
+
+    /**
+     * Permite modificar propuestas
+     * @param proposalTable tabla donde se visualizan las propuestas
+     * @param title         nombre de la propuesta
+     * @param description   descripción de la propuesta
+     * @param startDate     fecha de la propuesta
+     * @param indexEntity   indice del objeto del JCombobox
+     */
+    public void softDeleteProposal(JTable proposalTable, String title, String description, String startDate, int indexEntity, int idProposal) {
+        String sql = "UPDATE PROPOSALS SET status = ? WHERE idproposal = ?";
+
+        try {
+            // Almacenamos el nº total de filas que hay en la tabla
+            int totalRows = proposalTable.getRowCount();
+
+            // Almacena el nº de fila seleccionado
+            int selectedRow = proposalTable.getSelectedRow();
+
+            // Si no hay ninguna fila creada
+            if (totalRows == 0) {
+                throw new CustomException(1116);
+
+                // Si no hay ninguna fila seleccionada
+            } else if (selectedRow < 0) {
+                throw new CustomException(1114);
+
+                // Si los campos están vacíos
+            } else if (title.isEmpty() || description.isEmpty() || startDate.isEmpty() || indexEntity == -1) {
+                throw new CustomException(1111);
+
+            } else {
+                try (PreparedStatement stmt = DatabaseConnection.getConnection().prepareStatement(sql)) {
+
+                    stmt.setString(1, "inactive");
+
+                    stmt.setInt(2, idProposal);
+
+                    stmt.executeUpdate();
+                    //stmt.close(); // No hace falta con el try-with-resources
+
+                    // Añadimos la entrada al log
+                    Log.capturarRegistro("PROPOSAL DELETE " + title + " "
+                            + description + " "
+                            + InputOutput.stringToDate(startDate) + "  "
+                            + "ENTITY ID " + indexEntity);
+
+                    // Actualizamos datos de la tabla
+                    showData(proposalTable);
+
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+
+
+        } catch (CustomException ce) {
+            InputOutput.printAlert(ce.getMessage());
+
+            // Capturamos error para el registro
+            Error.capturarError("PROPOSAL " + ce.getMessage());
+        } catch (ParseException e) {
+            InputOutput.printAlert("Error: La fecha introducida no es válida");
+        }
+
+    }
+
+    /**
+     * Muestra los datos actualizados en la tabla de propuestas
+     *
+     * @param proposalTable tabla dónde se visualizan las propuestas creadas
+     */
+    public void showData(JTable proposalTable) {
+
+        // Instanceamos el objeto para cargar los datos de la bbdd
+        ProposalDB propdb = new ProposalDB();
+
+        String[] colIdentifiers = {"ID", "Título", "Descripción", "Fecha de inicio", "Entidad"};
+
+        // Añade los datos al modelo
+        proposalTable.setModel(new CustomTableModel(
+                propdb.listProposalsObject(),
+                colIdentifiers
+        ));
+
+        // Diseño básico de la tabla
+        CustomTableConfig.initConfig(proposalTable);
     }
 }
